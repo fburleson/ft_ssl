c93a7dce722d53ba9481842b71b79f7f29a9cb7d
Joel Burleson <joelburleson0104@gmail.com>
2024-11-16 18:02:52 +0100

    feat: rsa command: -text option

--- src/process/key_util.c
+++ src/process/key_util.c
@@ -1,5 +1,4 @@
 #include "ft_ssl.h"
-#include <stdio.h>
 
 priv_key_t parse_priv_key(const char *content)
 {
@@ -36,7 +35,7 @@ static void print_base64(const uint64_t a, const int fd)
     base64_t base64;
 
     base64 = dec_to_base64(a);
-    write(fd, base64, BASE64_N_DIGITS);
+    print_str_fd(base64, fd);
     free(base64);
 }
 
@@ -59,14 +58,37 @@ void print_pub_key(const pub_key_t *const pub_key, const int fd)
     print_str_fd("\n" PUB_KEY_END, fd);
 }
 
+static void print_base16(const uint64_t a, const int fd)
+{
+    base16_t base16;
+
+    base16 = dec_to_base16(a);
+    print_str_fd("0x", fd);
+    print_str_fd(base16, fd);
+    free(base16);
+}
+
+static void print_key_component(const char *name, const uint64_t value, const int fd)
+{
+    print_str_fd(name, fd);
+    print_str_fd(": ", fd);
+    print_base16(value, fd);
+    print_str_fd("\n", fd);
+}
+
 void print_priv_key_text(const priv_key_t *const priv_key, const int fd)
 {
     print_str_fd("private key: (64 bit, 2 primes)\n", fd);
-    printf("%lu\n", priv_key->mod);
+    print_key_component("mod", priv_key->mod, fd);
+    print_key_component("public exponent", priv_key->pub_exp, fd);
+    print_key_component("private exponent", priv_key->priv_exp, fd);
+    print_key_component("prime1", priv_key->p, fd);
+    print_key_component("prime2", priv_key->q, fd);
 }
 
 void print_pub_key_text(const pub_key_t *const pub_key, const int fd)
 {
     print_str_fd("public key: (64 bit)\n", fd);
-    printf("%lu\n", pub_key->mod);
+    print_key_component("mod", pub_key->mod, fd);
+    print_key_component("public exponent", pub_key->pub_exp, fd);
 }
