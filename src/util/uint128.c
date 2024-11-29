4f6168cbc07e437b2343ec838e3b75de1bc22e42
Joel Burleson <joelburleson0104@gmail.com>
2024-11-29 18:05:54 +0100

    128 bit modulo (64 bit only)

--- src/util/uint128.c
+++ src/util/uint128.c
@@ -1,24 +1,22 @@
 #include "ft_ssl.h"
 
-static uint128_t sub128_64(const uint128_t a, const uint64_t b)
+static uint128_t sub128_64(const uint128_t a, const uint128_t b)
 {
     bool      borrow;
     uint128_t difference;
 
-    difference.low  = a.low - b;
-    borrow          = a.low < b;
-    difference.high = a.high - borrow;
+    difference.low  = a.low - b.low;
+    borrow          = a.low < b.low;
+    difference.high = a.high - b.high - borrow;
     return difference;
 }
 
 uint64_t mod128(const uint128_t a, const uint64_t mod)
 {
-    uint128_t remainder;
+    __uint128_t remainder;
 
-    remainder = a;
-    while (remainder.high)
-        remainder = sub128_64(remainder, mod);
-    return remainder.low % mod;
+    remainder = (a.high << 64) | a.low;
+    return remainder % mod;
 }
 
 uint128_t prod128(uint64_t a, uint64_t b)
